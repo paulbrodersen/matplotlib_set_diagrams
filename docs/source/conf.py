aa964c89b321a84d3dfeb4af9daa1c555c5e6358
Paul Brodersen <paulbrodersen@gmail.com>
2024-05-09 16:11:34 +0100

    Setup sphinx extensions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,20 +9,64 @@
 project = 'Matplotlib set diagrams'
 copyright = '2024, Paul Brodersen'
 author = 'Paul Brodersen'
-release = '0.0.0'
+
+# The full version, including alpha/beta/rc tags
+import matplotlib_set_diagrams
+release = matplotlib_set_diagrams.__version__
+
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.viewcode',
+    'numpydoc',
+    'sphinx_gallery.gen_gallery',
+]
 
+autosummary_generate = True
+numpydoc_show_class_members = False
+
+# Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
 
+# -- Options for HTML output ---------------------------------------------------
 
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+# html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 
-html_theme = 'alabaster'
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+
+# -- Options for Sphinx Gallery ------------------------------------------------
+
+from sphinx_gallery.sorting import FileNameSortKey
+
+sphinx_gallery_conf = {
+    'examples_dirs': ['../../examples'],                                     # path to your example scripts
+    'gallery_dirs': ['sphinx_gallery_output', 'sphinx_gallery_animations'],  # path to where to save gallery generated output
+    'within_subsection_order': FileNameSortKey,
+    'matplotlib_animations': True,
+}
+
+# -- Suppress warnings --------------------------------------------------------
+
+import warnings
+warnings.filterwarnings("ignore", category=RuntimeWarning)
